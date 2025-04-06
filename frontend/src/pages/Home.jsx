1cae15efc9ed36173f64c913fbce832c8b925499
Abel Tsegaye <abeloy4@gmal.com>
2025-04-06 19:56:24 -0400

    Used the navbar and also created footer component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FiBell } from "react-icons/fi";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 export default function Home() {
   // Dummy data for demonstration
@@ -92,15 +93,7 @@ export default function Home() {
           </div>
         </div>
       </main>
-
-      {/* Footer */}
-      <footer className="bg-white border-t mt-8">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
-          <p className="text-center text-sm text-gray-500">
-            © 2025 Smart Booking System. All rights reserved.
-          </p>
-        </div>
-      </footer>
+      <Footer />
     </div>
   );
 }
